d5fa1b95f3f4dc5c771c817ad9b1311ae80c112c
Gi_Hoon <kiki01111@naver.com>
2023-09-22 15:22:51 +0900

    version1_0_0( AboutUs수정 )

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -4,6 +4,10 @@ function AboutUs() {
     return (
       <div>
         <h2>만든이들 소개 페이지입니다.</h2>
+        <div>
+          <p>노기훈</p>
+          <p>김대성</p>
+        </div>
       </div>
     );
   }
