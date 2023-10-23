1d776af27163384040f3cd8da91e3a8053441d42
Gi_Hoon <kiki01111@naver.com>
2023-10-23 23:27:10 +0900

    webGame off

--- src/Pages/ClubroomLocator.jsx
+++ src/Pages/ClubroomLocator.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-import { Unity, useUnityContext } from "react-unity-webgl";
+// import { Unity, useUnityContext } from "react-unity-webgl";
 
 const ClubroomLocator=()=>{
-  const { unityProvider } = useUnityContext({
-    loaderUrl: "Build/WebGL_Build.loader.js",
-    dataUrl: "Build/WebGL_Build.data",
-    frameworkUrl: "Build/WebGL_Build.framework.js",
-    codeUrl: "Build/WebGL_Build.wasm",
-  });
+  // const { unityProvider } = useUnityContext({
+  //   loaderUrl: "Build/WebGL_Build.loader.js",
+  //   dataUrl: "Build/WebGL_Build.data",
+  //   frameworkUrl: "Build/WebGL_Build.framework.js",
+  //   codeUrl: "Build/WebGL_Build.wasm",
+  // });
     return (
       <div className='Unity_Back'>
         <h1>게임방법: 화살표 조작</h1>
-        <Unity style={{
+        {/* <Unity style={{
             
             width:'65%',
             height:'65%',
             
             alignContent:'center'
 
-        }} unityProvider={unityProvider} />
+        }} unityProvider={unityProvider} /> */}
     </div>
     );
   };
