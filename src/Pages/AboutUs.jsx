2e0b21c2ccd899337d18a445ca05cba0766dca8d
Gi_Hoon <kiki01111@naver.com>
2023-10-26 00:29:03 +0900

    clubLocator 1차 개발완료(게임종료버튼 추가)

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -3,7 +3,7 @@ import '../Styles/AboutUs.css'
 function AboutUs() {
     return (
       <div className="AboutUs">
-        <h1>Member</h1>
+        <h1>Who made</h1>
         <div className="AboutUs_cardBundle">
           <div className="AboutUs_Card"><div className="Member_face"></div></div>
           <div className="AboutUs_Card"><div className="Member_face"></div></div>
--- src/Pages/ClubroomLocator.jsx
+++ src/Pages/ClubroomLocator.jsx
@@ -1,26 +1,43 @@
 import React from "react";
-// import { Unity, useUnityContext } from "react-unity-webgl";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import '../Styles/ClubroomLocator.css'
+const ClubroomLocator = () => {
+  const { unityContext, unityProvider } = useUnityContext({
+    loaderUrl: "Build/Build_Unity_WebGL.loader.js",
+    dataUrl: "Build/Build_Unity_WebGL.data",
+    frameworkUrl: "Build/Build_Unity_WebGL.framework.js",
+    codeUrl: "Build/Build_Unity_WebGL.wasm",
+  });
+  // useEffect(() => {
+  //   // Unity WebGL 게임이 언마운트될 때 실행할 종료 코드
+  //   return () => {
+  //     if (unityContext && unityContext.Quit) {
+  //       unityContext.Quit().then(() => {
+  //         console.log("Unity WebGL game has been successfully quit.");
+  //       });
+  //     }
+  //     else{
+  //       console.log("error!!!!!!!!!!!!!!!");
+  //     }
+  //   };
+  // }, [unityContext]);
 
-const ClubroomLocator=()=>{
-  // const { unityProvider } = useUnityContext({
-  //   loaderUrl: "Build/WebGL_Build.loader.js",
-  //   dataUrl: "Build/WebGL_Build.data",
-  //   frameworkUrl: "Build/WebGL_Build.framework.js",
-  //   codeUrl: "Build/WebGL_Build.wasm",
-  // });
-    return (
-      <div className='Unity_Back'>
-        <h1>게임방법: 화살표 조작</h1>
-        {/* <Unity style={{
-            
-            width:'65%',
-            height:'65%',
-            
-            alignContent:'center'
-
-        }} unityProvider={unityProvider} /> */}
+  return (
+    <div className="Unity_Back">
+      <h1>게임방법: 화살표 조작</h1>
+      <Unity
+        unityContext={unityContext}
+        style={{
+          width: "60%",
+          height: "60%",
+          alignContent: "center",
+          alignItems: "center",
+        }}
+        unityProvider={unityProvider}
+      />
     </div>
-    );
-  };
-  
-  export default ClubroomLocator;
+  );
+};
+
+export default ClubroomLocator;
+
