be6bd6bf891aaf36cf23973f5dfa79bfa92cb3f3
Gi_Hoon <kiki01111@naver.com>
2023-10-26 20:53:56 +0900

    2차 merge전 main push

--- src/Components/BestClub.jsx
+++ src/Components/BestClub.jsx
@@ -346,7 +346,7 @@ const academicClubs = [
     
                     break;
                 default:console.log("error");
-                break;;
+                break;
             }
         }
     };
